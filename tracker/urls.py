18ee661eb1b93c24df69bd9ba5d5abba1073e8ab
Najiya <najiyanasrin9496@gmail.com>
2025-03-04 13:29:55 +0530

    third

--- tracker/urls.py
+++ tracker/urls.py
@@ -4,5 +4,6 @@ from tracker import views
 
 urlpatterns = [
     path("create/",views.UserCreateView.as_view()),
-    path("meals/", views.MealListCreateView.as_view())
+    path("meals/", views.MealListCreateView.as_view()),
+    path("calorie/sum/",views.TotalCalorieView.as_view()),
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -3,6 +3,10 @@ from rest_framework.generics import CreateAPIView,ListCreateAPIView,ListAPIView
 from rest_framework import authentication, permissions
 from tracker.serializers import UserCreationSerializer,MealSerializer
 from tracker.models import Meal
+from rest_framework.views import APIView
+from django.db.models import Sum
+from rest_framework.response import Response
+
 
 class UserCreateView(CreateAPIView):
     serializer_class = UserCreationSerializer
@@ -18,4 +22,13 @@ class MealListCreateView(ListAPIView, CreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+
+class TotalCalorieView(APIView):
+    authentication_classes=[authentication.BasicAuthentication]
+    permission_classes=[permissions.IsAuthenticated]
+    def get(self,request,*args,**kwargs):
+
+        total_sum=Meal.objects.filter(user=request.user).values("calories").aggregate(total=Sum("calories"))
+        return Response(data=total_sum)
 
